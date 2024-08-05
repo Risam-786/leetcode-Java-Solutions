8348f0d9e6207c6738e46a524f99cf8c23704970
Fizul Risam S <risam2004@gmail.com>
2024-08-05 20:03:55 +0530

    Time: 2098 ms (17.3%), Space: 40.3 MB (51.01%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum=0;
+        for( int i=1; i<=num/2; i++)
+        {
+            if(num%i==0)
+            {
+                sum+=i;
+            }
+
+        }
+      if(sum==num)
+      {
+        return true;
+      }
+      return false;
+    }
+}
