ab969d9e6c8d4f8ed5a193fb9e32dc0b1ea7d875
Fizul Risam S <risam2004@gmail.com>
2024-08-15 14:02:57 +0530

    Time: 66 ms (20.35%), Space: 51 MB (94.51%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let present = init;
+    return{ increment:()=>++present,
+            decrement:()=>--present,
+            reset:()=>present = init,}
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
