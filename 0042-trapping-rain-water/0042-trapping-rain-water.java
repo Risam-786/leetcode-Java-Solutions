4b9d4bc19aad91a267cac3aa633a33c25abcbdf3
Fizul Risam S <risam2004@gmail.com>
2024-08-25 21:28:04 +0530

    Time: 1 ms (66.99%), Space: 45.9 MB (77.21%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int trap(int[] height) 
+    {
+        int n = height.length;
+        int [] prefix = new int[n];
+        int [] suffix = new int[n];
+       prefix[0] = height[0];
+       for(int i=1;i<=n-1;i++)
+       {
+        if(prefix[i-1]<height[i])
+        prefix[i] = height[i];
+        else
+        prefix[i]=prefix[i-1];
+       }
+         suffix[n-1] = height[n-1]; 
+        for(int i= n-2 ; i>=0 ; i--)
+        {
+            if(suffix[i+1] < height[i])
+            {
+                suffix[i] = height[i];
+            }
+            else 
+            {
+                suffix[i] = suffix[i+1];
+            }
+        }
+        int water = 0;
+        for(int i=0;i<=n-1;i++)
+        {
+            int min = (prefix[i]<=suffix[i])?prefix[i]:suffix[i];
+            water =water + (min-height[i]);
+        }
+        return water;
+    }
+}
