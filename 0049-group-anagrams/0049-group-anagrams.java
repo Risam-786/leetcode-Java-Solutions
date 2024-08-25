97e502c872a8e90d3b53b0280b7c9d56d20cfc54
Fizul Risam S <risam2004@gmail.com>
2024-08-25 20:59:05 +0530

    Time: 6 ms (97.79%), Space: 47.9 MB (32.55%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) 
+    {
+       Map<String,List<String>> map = new HashMap<>();
+       for(String str : strs){
+       char[] ch =str.toCharArray();
+       Arrays.sort(ch);
+       String s = new String(ch);
+       if(!map.containsKey(s)){
+        map.put(s,new ArrayList<>());
+       }
+       map.get(s).add(str);
+      
+    }
+    return new ArrayList<>(map.values());
+    }
+}
